48d9faa28464761ddeb3ea372d151cfdc001a95f
Leon Yalin <yelinichevl@gmail.com>
2020-10-13 00:25:44 +0300

    add oraginize_lerger_programs creating & using packages

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.getting_started.getting_started import getting_started_main 
+from src.organizing_larger_programs.organizing_larger_programs import organizing_larger_programs_main
 
 
 def main():
     getting_started_main()
+    organizing_larger_programs_main()
 
 if __name__ == "__main__":
     main()
---
+++ src/organizing_larger_programs/organizing_larger_programs.py
@@ -0,0 +1,46 @@
+from ..util import print_cmd
+
+
+def creating_packages():
+    print_cmd('Nesting modules', """
+    - In python, the app divided into packages with modules. Package in like a Java package (folder like), and a module is a single python file.
+    - Package has the __path__ property that holds the package path
+
+    - If the REPL doesn't recognize the path, we can add it manually: sys.path.append('folder_name')
+    - Other options is to expor it via bash variable: EXPORT PYTHONPATH=folder_name
+    """)
+
+
+def using_packages():
+    print_cmd('Using packages', """
+    The __file__ property holds the __init__.py file path.
+    When a package is imported, the __init__.py is being executed. We can see it by placing print statement there.
+    To run a module from a command line, use "python -m package_name.module_name"
+
+    Import synthax:
+    from package import module1, module2
+    from .. import module1
+
+    Using __all__
+    The __all__ contains list of all public exports of the module(without underscores) when import * is used
+    If not specified, all functions will be public
+    Hint: use print(locals()) to view all local available variables
+
+    Example of __init__.py with __all__:
+    from package.module import func1 as f1
+    from package2.module2 import func2 as f2
+    __all__ = ['f1', 'f2']
+    """)
+
+
+def namespaces_and_executable_packages():
+    print_cmd('Namspaces and executable packages', """
+    Namespace packages:
+    These packages are splitted across multiple files and do not have the __init__,py file.
+    """)
+
+
+def organizing_larger_programs_main():
+    creating_packages()
+    using_packages()
+    namespaces_and_executable_packages()
